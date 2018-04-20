ed553b731626a303b33fed4c8796f3ed4b248390
AFFINIONDS\rshah <rshah@cxloyalty.com>
2018-04-19 21:45:39 -0400

    another commit

--- springboot-sample-app/src/main/java/hello/Application.java
+++ springboot-sample-app/src/main/java/hello/Application.java
@@ -12,7 +12,7 @@ public class Application {
 
 	@RequestMapping("/")
 	public String home() {
-		return "Welcome to OpenShift";
+		return "Welcome to OpenShift. Welcome to Rolling Deployments";
 	}
 
 
